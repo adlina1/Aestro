901d9f559554ecc383b2b0c8a593d433cd717472
Adrien Linares <adlwqa@gmail.com>
2020-10-27 21:41:25 +0100

    v[0.2]

--- src/Router.php
+++ src/Router.php
@@ -1,5 +1,19 @@
 <?php
 
+require_once("view/View.php");
+
 class Router {
+	
+
+	public function main(){
+		
+			$view = new View();
+			$view->makeHomePage();
+			$view->render();
+
+
+			//$_GET pour switcher entre pages
+	}
+
 
 }
--- src/view/View.php
+++ src/view/View.php
@@ -1,5 +1,42 @@
 <?php
 
 class View {
+	
+	protected $title;
+	protected $content;
+	protected $style;
+
+	public function render(){
+
+	   	if ($this->title === null || $this->content === null) {
+                 $this->makeUnexpectedErrorPage();
+		}
+?>
+	<!DOCTYPE html>
+	<html lang="fr">
+	<head>
+	        <title><?php echo $this->title; ?></title>
+	        <meta charset="UTF-8" />
+	        <link rel="stylesheet" href="styles/style.css" />
+	        <style>
+				<?php echo $this->style; ?>
+	        </style>
+	</head>
+
+	<body>
+		<?php echo $this->content; ?>
+	</body>
+
+	</html>
+
+<?php } // render
+
+	public function makeHomePage(){
+		$this->title = "Aestro";
+		$this->content = "Test";
+	}
+
+
+
 
 }
